bc6dc4435b16718e927f120fd63b0aa567115346
Setyo Aryo Pangestu <setyoaryopangestu25@gmail.com>
2022-03-03 20:48:57 +0700

    Percobaan 3

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,20 @@ class DetailScreen extends StatelessWidget{
               ),
             ),//Title
             Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: const <Widget>[
+                      Icon(Icons.calendar_today),
+                      Text('Open Everyday'),
+                    ],
+                  )
+                ],
+              ),
+            ),//Logo
+            Container(
               padding: const EdgeInsets.all(16.0),
               child: const Text(
                 'Museum Inside a decommissioned Russion war submarine tours & an adjacent park with cafes. Clean and wel maintened. Car park cost 10k, enterance fee 15k/person. You can see KRI Pasopati there, it is a russian whisket class. You can also watch the video abaout the Indonesian Navy at the building beside the submarine',
