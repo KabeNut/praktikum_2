52fc88b554e05802b6910cef4d83b11c58f1dea2
Setyo Aryo Pangestu <setyoaryopangestu25@gmail.com>
2022-03-03 21:24:40 +0700

    Percobaan 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prak2/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,55 +15,6 @@ class MyApp extends StatelessWidget {
       title: 'Contacts',
       theme: ThemeData(),
       home: const DetailScreen(),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget{
-  const DetailScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(top: 16.0),
-              child: const Text(
-                "Surabaya Submarine Monument",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),//Title
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    children: const <Widget>[
-                      Icon(Icons.calendar_today),
-                      Text('Open Everyday'),
-                    ],
-                  )
-                ],
-              ),
-            ),//Logo
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: const Text(
-                'Museum Inside a decommissioned Russion war submarine tours & an adjacent park with cafes. Clean and wel maintened. Car park cost 10k, enterance fee 15k/person. You can see KRI Pasopati there, it is a russian whisket class. You can also watch the video abaout the Indonesian Navy at the building beside the submarine',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),//Description
-          ],
-        ),
-      ),
     );
   }
 }
