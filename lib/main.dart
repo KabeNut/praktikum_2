aa7d553b96560ca362a42f60cb017cdfbf321be0
Setyo Aryo Pangestu <setyoaryopangestu25@gmail.com>
2022-03-31 08:56:44 +0700

    Tugas Prak 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:prak2/main_screen.dart';
-import 'package:prak2/provider/done_tourism_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:tugas_frontend_5/pages/DetailPage.dart';
+import 'package:tugas_frontend_5/pages/FormCreate.dart';
+import 'package:tugas_frontend_5/pages/FormEdit.dart';
+import 'package:tugas_frontend_5/pages/LandingPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,13 +14,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => DoneTourismProvider(),
-      child: MaterialApp(
-        title: 'Contacts',
-        theme: ThemeData(),
-        home: MainScreen(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LandingPage(),
+        '/detail-page': (context) => DetailPage(),
+        '/form-create': (context) => FormCreate(),
+        '/form-edit': (context) => FormEdit(),
+      },
     );
   }
 }
